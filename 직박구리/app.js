91c98d24178dcfd150594d53d8f334670f7126ed
Terry Yun <nooheat1228@gmail.com>
2017-08-20 12:52:49 +0900

    Update app.js

--- 직박구리/app.js
+++ 직박구리/app.js
@@ -1,5 +1,4 @@
 var Botkit = require('botkit');
-<<<<<<< HEAD
 const client = require('./request/httpClient');
 const database = require('./database');
 const Product = require('./database/model/product');
@@ -14,18 +13,11 @@ const log = {};
 var bot = controller.spawn({
     token: reverseString('rJNds4poZpAjglzYx7TlOVCo-986652553822-bxox')
 });
-=======
-
-var controller = Botkit.slackbot();
-var key = "epKApMTcjyqeXUnl9ikCX6MS-650511922822-bxox";
-var bot = controller.spawn({ token: reverseString(key)});
->>>>>>> master
 
 function reverseString(str) {
     return str.split("").reverse().join("");
 }
 
-<<<<<<< HEAD
 bot.startRTM(function (err, bot, payload) {
     if (err) {
         console.log(err);
@@ -385,61 +377,3 @@ function sweet(bot, message, code, invoice) {
         })
     })
 }
-=======
-bot.startRTM(function(err,bot,payload) {
-    if (err) { 
-         throw new Error('Could not connect to Slack'); 
-    } 
-}); 
-
-controller.hears(["Hello","Hi"],["direct_message","direct_mention","mention","ambient"], function(bot,message) { 
-    bot.reply(message,'Hello, how are you today?'); 
-});
-
-controller.hears(["바지 보여줘"],["direct_message","direct_mention","mention","ambient"], function(bot,message) {
-    var resMessage = {
-    "text": "{0}개의 결과가 검색되었습니다",
-    "attachments": [
-        {
-            "fields": [
-                {
-                    "title": "상품 이름",
-                    "short": true
-                },
-                {
-                    "title": "가격",
-                    "short": true
-                }
-            ],
-            "image_url": "http://shopping.phinf.naver.net/main_1176561/11765612239.20170624195605.jpg?type=f140"
-        },
-        {
-            "fallback": "Would you recommend it to customers?",
-            "title": "장바구니에 담으시겠습니까?",
-            "callback_id": "comic_1234_xyz",
-            "color": "#9AA3E3",
-            "attachment_type": "default",
-            "actions": [
-                {
-                    "name": "recommend",
-                    "text": "담기",
-                    "type": "button",
-                    "value": "recommend"
-                },
-                {
-                    "name": "no",
-                    "text": "취소",
-                    "type": "button",
-                    "value": "bad"
-                }
-            ]
-        }
-    ]
-}; 
-    bot.reply(message,resMessage); 
-});
-
-controller.hears(["덥다","더워","더워요"],["direct_message","direct_mention","mention","ambient"],function(bot,message) { 
-    bot.reply(message,'진짜 더워요'); 
-});
->>>>>>> master
