611a1a044d5d8b42a1dcb469f2621292f1604c05
Terry Yun <nooheat1228@gmail.com>
2017-08-20 13:11:08 +0900

    구매내역이 없을 때 처리

--- bot-server/app.js
+++ bot-server/app.js
@@ -266,6 +266,9 @@ controller.hears(["구매내역", /구매내역/mg], ["direct_message", "direct_
                 }
             }
         })
+        .catch((err) => {
+            bot.reply(message, err.message);
+        })
 });
 
 controller.hears(["배송추적", /^배송추적/], ["direct_message", "direct_mention", "mention"], function (bot, message) {
@@ -378,59 +381,56 @@ function sweet(bot, message, code, invoice) {
     })
 }
 
-bot.startRTM(function(err,bot,payload) {
-    if (err) { 
-         throw new Error('Could not connect to Slack'); 
-    } 
-}); 
-
-controller.hears(["Hello","Hi"],["direct_message","direct_mention","mention","ambient"], function(bot,message) { 
-    bot.reply(message,'Hello, how are you today?'); 
+bot.startRTM(function (err, bot, payload) {
+    if (err) {
+        throw new Error('Could not connect to Slack');
+    }
 });
 
-controller.hears(["바지 보여줘"],["direct_message","direct_mention","mention","ambient"], function(bot,message) {
+controller.hears(["Hello", "Hi"], ["direct_message", "direct_mention", "mention", "ambient"], function (bot, message) {
+    bot.reply(message, 'Hello, how are you today?');
+});
+
+controller.hears(["바지 보여줘"], ["direct_message", "direct_mention", "mention", "ambient"], function (bot, message) {
     var resMessage = {
-    "text": "{0}개의 결과가 검색되었습니다",
-    "attachments": [
-        {
-            "fields": [
-                {
-                    "title": "상품 이름",
-                    "short": true
-                },
-                {
-                    "title": "가격",
-                    "short": true
-                }
-            ],
-            "image_url": "http://shopping.phinf.naver.net/main_1176561/11765612239.20170624195605.jpg?type=f140"
-        },
-        {
-            "fallback": "Would you recommend it to customers?",
-            "title": "장바구니에 담으시겠습니까?",
-            "callback_id": "comic_1234_xyz",
-            "color": "#9AA3E3",
-            "attachment_type": "default",
-            "actions": [
-                {
-                    "name": "recommend",
-                    "text": "담기",
-                    "type": "button",
-                    "value": "recommend"
-                },
-                {
-                    "name": "no",
-                    "text": "취소",
-                    "type": "button",
-                    "value": "bad"
-                }
-            ]
-        }
-    ]
-}; 
-    bot.reply(message,resMessage); 
+        "text": "{0}개의 결과가 검색되었습니다",
+        "attachments": [{
+                "fields": [{
+                        "title": "상품 이름",
+                        "short": true
+                    },
+                    {
+                        "title": "가격",
+                        "short": true
+                    }
+                ],
+                "image_url": "http://shopping.phinf.naver.net/main_1176561/11765612239.20170624195605.jpg?type=f140"
+            },
+            {
+                "fallback": "Would you recommend it to customers?",
+                "title": "장바구니에 담으시겠습니까?",
+                "callback_id": "comic_1234_xyz",
+                "color": "#9AA3E3",
+                "attachment_type": "default",
+                "actions": [{
+                        "name": "recommend",
+                        "text": "담기",
+                        "type": "button",
+                        "value": "recommend"
+                    },
+                    {
+                        "name": "no",
+                        "text": "취소",
+                        "type": "button",
+                        "value": "bad"
+                    }
+                ]
+            }
+        ]
+    };
+    bot.reply(message, resMessage);
 });
 
-controller.hears(["덥다","더워","더워요"],["direct_message","direct_mention","mention","ambient"],function(bot,message) { 
-    bot.reply(message,'진짜 더워요'); 
+controller.hears(["덥다", "더워", "더워요"], ["direct_message", "direct_mention", "mention", "ambient"], function (bot, message) {
+    bot.reply(message, '진짜 더워요');
 });
